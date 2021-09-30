44e8f0595172d5ecd3754861f38b64dd87e86b1b
deividy <deividyferreira.n@gmail.com>
2021-09-29 21:37:04 -0300

    add components

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,20 @@
+import React, { InputHTMLAttributes } from "react";
+import { Container } from "../Input/styles";
+import { IconBaseProps } from 'react-icons'
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+    name: string;
+    icon: React.ComponentType<IconBaseProps>;
+}
+
+
+
+const Input:React.FC<InputProps> = ({ icon: Icon, ...rest }) => (
+    <Container>
+        { Icon && <Icon size={20} /> }
+        <input { ...rest }/>
+    </Container>
+)
+
+
+export default Input;
--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,7 +1,9 @@
 import React from "react";
-import { FiLogIn } from 'react-icons/fi'
+import { FiLogIn, FiMail, FiLock } from 'react-icons/fi'
 import { Container, Content, Background } from './styles'
-import logoImg from '../../assets/logo.svg'
+import logoImg from '../../assets/logo.svg';
+import Input from "../../components/Input";
+import Button from "../../components/Button";
 
 const SignIn: React.FC = () => (
     <Container>
@@ -9,9 +11,9 @@ const SignIn: React.FC = () => (
         <img src={logoImg} alt="Gobarber"></img>
         <form>
             <h1>Faça seu logon</h1>
-            <input type="text" placeholder="E-mail" />
-            <input type="password" placeholder="Senha" />
-            <button type="submit"> Entrar</button>
+            <Input name="email" icon={FiMail} type="text" placeholder="E-mail" />
+            <Input name="password" icon={FiLock} type="password" placeholder="Senha" />
+            <Button type="submit"> Entrar</Button>
             <a href="forgot">Esqueci minha senha</a>
         </form>
         <a>
