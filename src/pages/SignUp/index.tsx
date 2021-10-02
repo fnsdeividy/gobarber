4a54484d5017fb341deb48906b3f49228546e3a5
deividy <deividyferreira.n@gmail.com>
2021-10-01 21:46:28 -0300

    show errors input

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,31 +1,45 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useRef } from "react";
 import { FiArrowLeft, FiMail,FiUser, FiLock } from 'react-icons/fi';
 import { Container, Content, Background } from './styles';
 import logoImg from '../../assets/logo.svg';
 import Input from "../../components/Input";
 import Button from "../../components/Button";
 import { Form } from "@unform/web";
+import { FormHandles } from "@unform/core";
 import * as Yup from 'yup'
+import getValidationErrors from "../../utils/getValidationErrors";
+
 
 const SignUp: React.FC = () => {
+    const formRef = useRef<FormHandles>(null)
     const handleSubmit= useCallback( async (data:object)=> {
         try {
+
+           formRef.current?.setErrors({})
+
             const schema = Yup.object().shape({
                 name: Yup.string().required('Nome obrigatório') ,
+
                 email: Yup.string().required('E-mail obrigatório').email('Digite um E-mail válido'),
+                
                 password: Yup.string().min(6, 'No minimo 6 digitos'), 
             });
             
-            await schema.validate(data, { 
-                abortEarly:false
-            }  
-            )
             
-        } catch (err) {
-            console.log(err)
+            await schema.validate(data, {
+                 abortEarly:false,
+            })
+           
+            
+        } catch (err:any)  {
+           
+           const errors = getValidationErrors(err);
+           formRef.current?.setErrors(errors)
+            
         }
-    },[]);
+    }, []);
     
+   
     
     return (
         <Container>
@@ -34,7 +48,9 @@ const SignUp: React.FC = () => {
         
 
         <img src={logoImg}alt="Gobarber"></img>
-        <Form onSubmit={handleSubmit}>
+        <Form
+        ref={formRef} 
+        onSubmit={handleSubmit}>
             
             <h1>Faça seu Cadastro</h1>
             <Input 
@@ -55,6 +71,7 @@ const SignUp: React.FC = () => {
             type="password" 
             placeholder="Senha" 
             />
+            
             <Button type="submit"> Cadastrar </Button>
             
         </Form>
