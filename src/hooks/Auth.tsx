abc2d4a5737880cbde89cc06601126536b5a6110
deividy <deividyferreira.n@gmail.com>
2021-10-30 00:03:07 -0300

    Criando Header

--- src/hooks/Auth.tsx
+++ src/hooks/Auth.tsx
@@ -1,78 +1,81 @@
 import React, { createContext, useCallback, useState, useContext } from "react";
-import api from '../services/ApiClient'
+import api from "../services/ApiClient";
 
-interface AuthState {
-    token: string;
-    user: object;
+interface User {
+  id: string;
+  name: string;
+  avatar_url: string;
 }
 
+interface AuthState {
+  token: string;
+  user: User;
+}
 
 interface SignInCredentials {
-    email: string;
-    password: string;
+  email: string;
+  password: string;
 }
 
 interface AuthContextData {
-    user: object;
-    signIn(credentials:SignInCredentials): Promise<void>;
-    signOut(): void
+  user: User;
+  signIn(credentials: SignInCredentials): Promise<void>;
+  signOut(): void;
 }
 
- const AuthContext = createContext<AuthContextData>({} as AuthContextData);
-   
+const AuthContext = createContext<AuthContextData>({} as AuthContextData);
 
-  const AuthProvider: React.FC = ({ children }) => {
-    const [data, setData] = useState<AuthState>(() => {
-        const token = localStorage.getItem('@Gobarber:token');
-       const user = localStorage.getItem('@Gobarber:user');
+const AuthProvider: React.FC = ({ children }) => {
+  const [data, setData] = useState<AuthState>(() => {
+    const token = localStorage.getItem("@Gobarber:token");
+    const user = localStorage.getItem("@Gobarber:user");
 
-       if(token && user) {
-           return {
-               token,
-               user: JSON.parse(user)
-           }
-
-       }
-       return {} as AuthState
-    })
-
-    const signIn = useCallback(async ({ user, token, email, password })=> {
-        const res = await api.post('sessions', {
-            email,
-            password,
-            token,
-            user
-        })
-        token = res.data.token
-        user = res.data.user
-
-       localStorage.setItem('@Gobarber:token', token)
-       localStorage.setItem('@Gobarber:user', JSON.stringify(user))
-
-       setData({ token, user })
-    },[])
-
-    const signOut = useCallback(() => {
-        localStorage.removeItem('@Gobarber:token');
-        localStorage.removeItem('@Gobarber:user');
-
-        setData({} as AuthState)
-    }, [])
-
-    return (
-        <AuthContext.Provider value={{user: data.user, signIn, signOut}}>
-            { children }
-        </AuthContext.Provider>
-    )
-}
-
-    function useAuth(): AuthContextData {
-        const context = useContext(AuthContext) 
-
-        if(!context) {
-            throw new Error('useAuth must be used within an AuthProvider')
-        }
-
-        return context
+    if (token && user) {
+      return {
+        token,
+        user: JSON.parse(user),
+      };
     }
-export {  AuthProvider, useAuth }
+    return {} as AuthState;
+  });
+
+  const signIn = useCallback(async ({ user, token, email, password }) => {
+    const res = await api.post("sessions", {
+      email,
+      password,
+      token,
+      user,
+    });
+    token = res.data.token;
+    user = res.data.user;
+
+    localStorage.setItem("@Gobarber:token", token);
+    localStorage.setItem("@Gobarber:user", JSON.stringify(user));
+
+    setData({ token, user });
+  }, []);
+
+  const signOut = useCallback(() => {
+    localStorage.removeItem("@Gobarber:token");
+    localStorage.removeItem("@Gobarber:user");
+
+    setData({} as AuthState);
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user: data.user, signIn, signOut }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+function useAuth(): AuthContextData {
+  const context = useContext(AuthContext);
+
+  if (!context) {
+    throw new Error("useAuth must be used within an AuthProvider");
+  }
+
+  return context;
+}
+export { AuthProvider, useAuth };
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,5 +1,35 @@
 import React from "react";
+import { Container, Header, HeaderContent, Profile } from "./styles";
+import LogoImg from "../../assets/logo.svg";
+import { FiPower } from "react-icons/fi";
+import { useAuth } from "../../hooks/Auth";
 
-const Dashboard: React.FC = () => <h1>DashBoard</h1>
+const Dashboard: React.FC = () => {
+    const { signOut, user } = useAuth()
+
+  return (
+    <Container>
+      <Header>
+        <HeaderContent>
+          <img src={LogoImg} alt="Gobarber" />
+          <Profile>
+            <img
+              src={user.avatar_url }
+              alt={user.name}
+            />
+            <div>
+              <span>Bem-vindo,</span>
+              <strong>{user.name}</strong>
+            </div>
+          </Profile>
+
+          <button type="button" onClick={signOut}>
+            <FiPower />
+          </button>
+        </HeaderContent>
+      </Header>
+    </Container>
+  );
+};
 
 export default Dashboard;
